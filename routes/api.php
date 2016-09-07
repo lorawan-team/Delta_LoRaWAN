6e35de9d95e1e46e9fb5dbb333be66225eee332d
Jesse Labruyere <jesselabruyere@gmail.com>
2016-09-07 15:12:05 +0200

    Added example classes for the repository design pattern

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,24 @@ use Illuminate\Http\Request;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+/*Route::group(
+    [
+        'namespace' => '',
+        'prefix' => '/example',
+        'middleware => '',
+    ],
+    function () {
+        Route::get('/', [
+            'as' => 'example.show',
+            'uses' => 'ExampleController@show'
+        ]);
+    }
+);*/
+
+Route::resource('/example', 'ExampleController', [
+    'only' => ['index'],
+    'names' => [
+        'index' => 'example.index',
+    ]
+]);
