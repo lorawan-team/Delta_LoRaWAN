5077cd84efd7701ea2b61229549bbe28e47cf5df
Mark Dingemanse <dingemansemark@gmail.com>
2016-09-10 11:03:45 +0000

    added dingo to composer

--- config/app.php
+++ config/app.php
@@ -171,7 +171,7 @@ return [
          * Package Service Providers...
          */
 
-        //
+        Dingo\Api\Provider\LaravelServiceProvider::class,
 
         /*
          * Application Service Providers...
