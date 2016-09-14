a34ea2c15dfba5047decc209d369a03e11136a9c
Jesse Labruyere <jesselabruyere@gmail.com>
2016-09-14 10:54:57 +0200

    Added delta_service specific migrations

--- config/delta_service.php
+++ config/delta_service.php
@@ -8,7 +8,7 @@ return [
     |--------------------------------------------------------------------------
     */
 
-    'delta_service' => [
+    'connection' => [
         'driver'    => env('DB_CONNECTION', 'mysql'),
         'host'      => env('DB_HOST', 'localhost'),
         'database'  => env('DB_DATABASE', 'homestead'),
