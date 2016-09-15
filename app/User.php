1be5b63a501965b739cab5cfab6f8d8d8d168ea9
Jesse Labruyere <jesselabruyere@gmail.com>
2016-09-15 10:00:48 +0200

    WIP

--- app/User.php
+++ app/User.php
@@ -26,4 +26,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function devices()
+    {
+        return $this->hasMany('Delta\DeltaService\Devices\DeviceModel', 'device_id', 'users_id');
+    }
+
+    public function devices()
+    {
+        return $this->hasMany('Delta\DeltaService\Devices\DeviceModel', 'device_id', 'users_id');
+    }
 }
