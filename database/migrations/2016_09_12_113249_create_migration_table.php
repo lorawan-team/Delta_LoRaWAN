d724e81dc1511a34400efb7ac3495b64e68c1e5d
Leander Molegraaf <leandermolegraaf@gmail.com>
2016-09-19 13:57:02 +0200

    sensor_data tabel hernoemd naar migrations

--- database/migrations/2016_09_12_113249_create_migration_table.php
+++ database/migrations/2016_09_12_113249_create_migration_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateSensorDataTable extends Migration {
+class CreateMigrationsTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,7 +12,7 @@ class CreateSensorDataTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('sensor_data', function(Blueprint $table)
+		Schema::create('migrations', function(Blueprint $table)
 		{
             $table->increments('id');
 			$table->integer('sensor_id')->unsigned()->index('fk_sensor_data_sensor1_idx');
