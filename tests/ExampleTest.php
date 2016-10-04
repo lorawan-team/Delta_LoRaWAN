0874879cf03d516cb6d80af536eaebd0bb3808f0
Leander Molegraaf <leandermolegraaf@gmail.com>
2016-10-04 09:47:13 +0200

    added a basic unit testing skeleton

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,26 +2,81 @@
 
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
+
+    //use DatabaseMigrations;
+    //use WithoutMiddleware;
+
     public function setup()
     {
         parent::setUp();
-
-//        Route::enableFilters();
-
-//        Auth::loginUsingId(1);
+        Auth::loginUsingId(1);
     }
+
+    //roles - success
+
     /**
-     * A basic functional test example.
-     *
-     * @return void
+     * List all available rols
      */
-    public function testBasicExample()
+    public function testListRoles()
     {
-        $response = $this->call('GET', '1/measurements?account_id=1');
-        $this->assertNotEmpty($response);
+        $this->json('GET', '/roles?account_id=1')
+            ->assertResponseStatus(200)
+            ->seeJsonEquals([
+                'data' => [
+                    'id' => 1,
+                    'name' => 'DEVICE1'
+                ]
+            ]);
+
+        // @TODO check for structure and available roles
     }
+
+    //roles - failure
+
+    //devices - success
+
+    //devices - failure
+
+    //sensors - success
+
+    //sensors - failure
+
+    //measurements - success
+
+    //measurements - failure
+
+    //Generic tests
+    /**
+     * Test if an error is thrown when a request is made without an account id
+     *
+     *
+     * @TODO AuthToken is disabled during testing, in order to make tests work. this makes this test unusable
+     */
+    public function testFailWithoutAccountId()
+    {
+//        $this->json('GET', '/device')
+//            ->assertResponseStatus(400)
+//            ->seeJson([
+//            'message' => "No account ID included with request",
+//        ]);
+    }
+
+    /**
+     * Test if an error is thrown when the given account ID is invalid
+     */
+    public function testFailWithInvalidAccountId()
+    {
+
+        // TODO not yet implemented!
+
+//        $this->json('GET', '/device?account_id=99999999999999999')->assertResponseStatus(400)->seeJson([
+//            'message' => "No account ID included with request",
+//        ]);
+    }
+
+    //different sessions!
+
 }
